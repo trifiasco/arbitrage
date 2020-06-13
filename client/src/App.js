191179a71b38d23c325536503d3ca76c7e0333de
Arnab Roy <trifiasco@gmail.com>
2020-06-13 14:45:45 +0600

    Configure one single serving entry point

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,10 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const fetchTest = () => {
+    fetch('/users')
+    .then(res => console.log(res));
+  }
   return (
     <div className="App">
       <header className="App-header">
@@ -18,6 +22,7 @@ function App() {
         >
           Learn React
         </a>
+        <button onClick={fetchTest}>Fetch Test</button>
       </header>
     </div>
   );
--- server/app.js
+++ server/app.js
@@ -17,13 +17,13 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '../client/build')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 app.get('*', (req, res) => {
-  res.send('hello world')
+  res.send(express.static(path.join(__dirname, '../client/build/index.html')))
 })
 
 // catch 404 and forward to error handler
